982eac180f9559baa8f075120f231a3991461c7e
marionc22 <103053810+marionc22@users.noreply.github.com>
2022-10-11 10:39:31 +0200

    Update address.java

--- address.java
+++ address.java
@@ -1,5 +1,13 @@
-public class Adress {
+public class Address {
     private String number;
-    private String text;
-    private Date createAt;
+    private String street;
+    private String city;
+    private String country;
+
+    public Address(String number , String street, String city, String country) {
+        this.number = number; 
+        this.street = street; 
+        this.city = city; 
+        this.country = country;
+    }
 }
